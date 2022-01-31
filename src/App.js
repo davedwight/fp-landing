2cf8bc02311e2f44f8893d6c3f7fd9e3106104ab
davedwight <dpdwight@gmail.com>
2022-01-30 17:50:51 -0700

    initializes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./fp-logo-large.png";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <main className="App">
+            <body>
+                <section>
+                    <img src={logo} alt="large-logo" />
+                    <p>
+                        Family Promise Service Tracker is under construction.
+                        Please try again soon.
+                    </p>
+                </section>
+            </body>
+        </main>
+    );
 }
 
 export default App;
